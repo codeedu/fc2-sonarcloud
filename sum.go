0c034a996ceaf964ac0927a0e58001044a9b8bca
Wesley Willians <wesleywillians@gmail.com>
2020-12-16 13:15:23 -0500

    ci: Check if quality gate is working on sonar cloud

--- sum.go
+++ sum.go
@@ -10,14 +10,14 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
 
-func times(a int, b int) int {
-	return a * b
-}
+// func times(a int, b int) int {
+// 	return a * b
+// }
 
-func sumX(a int, b int) int {
-	return a + b + a
-}
+// func sumX(a int, b int) int {
+// 	return a + b + a
+// }
